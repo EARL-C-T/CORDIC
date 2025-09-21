7d54790a384e00a42225d58314eecf702291f65d
EARL T <adhoc_vor@proton.me>
2025-09-21 13:02:08 -0400

    Delete CORDICcmd/qk_sct.c
    more junk sorry really i should make this just CORDIC_CAL may do that later

--- CORDICcmd/qk_sct.c
+++
@@ -1,9 +0,0 @@
-#include "CORDIC.h"
-#include "fixpnt.h"
-#include "CORDIC_tbls.h"
-1
-	quiksc* q ;
-	q = quicksn(ang);
-	printf("sin:  %f cos: %f tan: %f \n",to_float(q->sin_aprox), to_float(q->cos_aprox), to_float(q->tan_aprox));
-	free(q);
-}
